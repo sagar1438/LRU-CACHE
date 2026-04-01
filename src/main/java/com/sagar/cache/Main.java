d354119bc67836d5034ff3e7f153c4eb8ae4dd24
Sagar Panwar <sagarpanwer11@gmail.com>
2026-04-01 21:59:17 +0530

    Update Main.java

--- src/main/java/com/sagar/cache/Main.java
+++ src/main/java/com/sagar/cache/Main.java
@@ -6,38 +6,38 @@ public class Main {
         LRU lru = new LRU(2);
         lru.put(1, 10);
         lru.put(2, 20);
-        System.out.println(lru.get(1)); // 10
-        lru.put(3, 30); // evicts key 2
-        System.out.println(lru.get(2)); // -1
-        lru.put(4, 40); // evicts key 1
-        System.out.println(lru.get(1)); // -1
-        System.out.println(lru.get(3)); // 30
-        System.out.println(lru.get(4)); // 40
+        System.out.println(lru.get(1)); 
+        lru.put(3, 30); 
+        System.out.println(lru.get(2)); 
+        lru.put(4, 40); 
+        System.out.println(lru.get(1)); 
+        System.out.println(lru.get(3)); 
+        System.out.println(lru.get(4)); 
 
         System.out.println("\n---- MemorySensitiveLRU ----");
         MemorySensitiveLRU memLRU = new MemorySensitiveLRU(2, 25);
         memLRU.put(1, 10, 10);
         memLRU.put(2, 20, 12);
-        memLRU.put(3, 30, 12); // exceeds memory (10+12+12=34 > 25), evicts 1
-        System.out.println(memLRU.get(1)); // -1
-        System.out.println(memLRU.get(2)); // 20
-        System.out.println(memLRU.get(3)); // 30
+        memLRU.put(3, 30, 12); 
+        System.out.println(memLRU.get(1)); 
+        System.out.println(memLRU.get(2)); 
+        System.out.println(memLRU.get(3)); 
 
         System.out.println("\n---- ConcurrentLRU ----");
         ConcurrentLRU concLRU = new ConcurrentLRU(2);
         concLRU.put(1, 10);
         concLRU.put(2, 20);
-        System.out.println(concLRU.get(1)); // 10
-        concLRU.put(3, 30); // evicts 2
-        System.out.println(concLRU.get(2)); // -1
+        System.out.println(concLRU.get(1)); 
+        concLRU.put(3, 30); 
+        System.out.println(concLRU.get(2)); 
 
         System.out.println("\n---- ConcurrentMemorySensitiveLRU ----");
         ConcurrentMemorySensitiveLRU concMemLRU = new ConcurrentMemorySensitiveLRU(2, 25);
         concMemLRU.put(1, 10, 10);
         concMemLRU.put(2, 20, 12);
-        concMemLRU.put(3, 30, 12); // evicts 1
-        System.out.println(concMemLRU.get(1)); // -1
-        System.out.println(concMemLRU.get(2)); // 20
-        System.out.println(concMemLRU.get(3)); // 30
+        concMemLRU.put(3, 30, 12); 
+        System.out.println(concMemLRU.get(1)); 
+        System.out.println(concMemLRU.get(2)); 
+        System.out.println(concMemLRU.get(3)); 
     }
 }
